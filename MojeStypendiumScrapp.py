11a1e8f28b54909bed4d2d973ba37d753201cc16
BednarekBartek <btbednarek2805@gmail.com>
2022-11-09 21:38:29 +0100

    Wyświetlanie pierwszego elementu z kazdej strony

--- MojeStypendiumScrapp.py
+++ MojeStypendiumScrapp.py
@@ -1,51 +1,34 @@
-#https://www.mojestypendium.pl/znajdz-{searching_item}/page/{page_number}/
-
-#Szukamy tytulu, organizatora, rodzaj, grupy docelowej, zasieg. termin, opis
-
 from bs4 import BeautifulSoup
 import requests
-import re
 
-title = None
-organizator = None
-szczegoly = None
-termin = None
-
-searching_topic = input('staz/konkurs/stypendium: ')
-
-url = f'https://www.mojestypendium.pl/znajdz-{searching_topic}/'
+url = f"https://www.mojestypendium.pl/znajdz-stypendium"
 page = requests.get(url)
 doc = BeautifulSoup(page.content, 'html.parser')
 
 page_text = list(doc.find(class_='page-numbers').next_siblings)
-pages = int(str(page_text[-4].text))
-
-informacje = {}
+pages = int(page_text[-4].text)
 
 for page in range(1, pages + 1):
-    url = f'https://www.mojestypendium.pl/znajdz-{searching_topic}/page/{page}'
+    url = f"https://www.mojestypendium.pl/znajdz-stypendium/page/{page}"
     page = requests.get(url)
     doc = BeautifulSoup(page.content, 'html.parser')
 
-    div = doc.find(class_='columns col-80')
+    title = doc.find(class_='title')
+    organizator = doc.find(class_='organizator-title')
+    rodzaj = doc.find('span', class_='taxonomy-list')
+    grupa_docelowa = doc.find('span', class_='taxonomy-list w2')
+    content = doc.find(class_='column second-col')
+    termin = content.find('span', class_='')
 
-    for item in div:
+    print('Tytuł: ' + title.text.strip())
+    print('Organizator: ' + organizator.text.strip())
+    print('Grupa docelowa: ' + grupa_docelowa.text.strip())
+    print('Termin składania wniosków: ' + termin.text.strip())
+    print('=================================================')
 
-        try:
-            title = div.find(class_='title').string
-            organizator = div.find(class_='organizator-title').string
-            termin = div.find('span', class_='').string
 
-            informacje[item] = {
-                'Tytuł': title.replace("\n", '').replace("\r", ''),
-                'Organizator': organizator.replace("\n", '').replace("\r", '').strip(),
-                'Termin': termin.replace("\n", '').replace("\r", '').strip()
-            }
-        except:
-            pass
 
-    print(informacje[item]['Tytuł'])
-    print(informacje[item]['Organizator'])
-    print(informacje[item]['Termin'])
-    print('==============================')
+
+
+
 
