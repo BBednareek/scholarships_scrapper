21b82329940afc95b964898df1175f54ff0972df
BednarekBartek <btbednarek2805@gmail.com>
2022-11-23 16:25:03 +0100

    MojeStypendia done

---
+++ Executable/Staze_z_moje_stypendia.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+from Functions.createStaze import *
+
+url = f"https://www.mojestypendium.pl/znajdz-staz"
+page = requests.get(url)
+doc = BeautifulSoup(page.content, 'html.parser')
+
+page_text = list(doc.find(class_='page-numbers').next_siblings)
+pages = int(page_text[-4].text) #Szukamy ilosci stron
+
+for page in range(1, pages + 1): #Przechodzimy przez kazda strone
+    url = f"https://www.mojestypendium.pl/znajdz-staz/page/{page}"
+    page = requests.get(url)
+    doc = BeautifulSoup(page.content, 'html.parser')
+
+    #Materialy do scrapowania
+    tytul = doc.find_all(class_='title')
+    organizator = doc.find_all(class_='organizator-title')
+    rodzaj = doc.find_all('div', class_='fleft', string="Rodzaj:")
+    grupa = doc.find_all('span', class_='taxonomy-list w2')
+    zasieg = doc.find_all('div', class_='fleft', string="Zasięg:")
+    branza = doc.find_all('div', class_='fleft', string="Branża:")
+    link = doc.find_all(class_='hide-for-large anchor')
+
+    #Przechodzimy przez kazdy element i wrzucamy go do bazy
+    for i in range(min(len(tytul), len(grupa))):
+        t = tytul[i].text.strip().replace('\\n', '').replace('\\r', '')
+        o = organizator[i].text.strip().replace('\\n', '').replace('\\r', '')
+        r = rodzaj[i].find_next().text.strip().replace('\\n', '').replace('\\r', '')
+        g = grupa[i].text.strip().replace('\\n', '').replace('\\r', '')
+        z = zasieg[i].find_next().text.strip().replace('\\n', '').replace('\\r', '')
+        b = branza[i].find_next().text.strip().replace('\\n', '').replace('\\r', '')
+        l = link[i]['href'].strip().replace('\\n', '').replace('\\r', '')
+
+        appendStaze(t, o, r, z, g, b, l)
+
+
+
---
+++ Functions/createKonkursy.py
@@ -0,0 +1,37 @@
+import mysql.connector
+
+conn = mysql.connector.connect(host='localhost', port='3306', user='root', password='', database='baza_danych')
+cursor = conn.cursor()
+
+query = "SHOW TABLES LIKE 'konkursy'"
+cursor.execute(query)
+result = cursor.fetchall()
+
+if result:
+    query = "DROP TABLE konkursy"
+    cursor.execute(query)
+    conn.commit()
+
+    query = "CREATE TABLE konkursy (id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, tytul varchar(500) NOT NULL, organizator varchar(100) NOT NULL, rodzaj varchar(50) NOT NULL, zasieg varchar(50) NOT NULL, grupa_docelowa varchar(100) NOT NULL, termin varchar(20) NOT NULL, link varchar(1000) NOT NULL)"
+    cursor.execute(query)
+    conn.commit()
+    def appendKonkursy(tytul: str, organizator: str, rodzaj: str, zasieg: str, grupa: str, termin: str, link: str):
+        try:
+            query = "INSERT INTO konkursy (tytul, organizator, rodzaj, zasieg, grupa_docelowa, termin, link) values(%s, %s, %s, %s, %s, %s, %s)"
+            values = (tytul, organizator, rodzaj, zasieg, grupa, termin, link)
+            cursor.execute(query, values)
+            conn.commit()
+        except:
+            print("Błąd")
+else:
+    query = "CREATE TABLE konkursy (id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, tytul varchar(500) NOT NULL, organizator varchar(100) NOT NULL, rodzaj varchar(50) NOT NULL, zasieg varchar(50) NOT NULL, grupa_docelowa varchar(100) NOT NULL, termin varchar(20) NOT NULL, link varchar(1000) NOT NULL)"
+    cursor.execute(query)
+    conn.commit()
+    def appendKonkursy(tytul: str, organizator: str, rodzaj: str, zasieg: str, grupa: str, termin: str, link: str):
+        try:
+            query = "INSERT INTO konkursy (tytul, organizator, rodzaj, zasieg, grupa_docelowa, termin, link) values(%s, %s, %s, %s, %s, %s, %s)"
+            values = (tytul, organizator, rodzaj, zasieg, grupa, termin, link)
+            cursor.execute(query, values)
+            conn.commit()
+        except:
+            print("Błąd")
